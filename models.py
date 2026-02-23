b78d92a00d5a52a4393f108d8d02c2171961d887
pepeuloureirob <pepeu.loureirob@gmail.com>
2026-02-22 23:40:18 -0300

    Update models.py

--- models.py
+++ models.py
@@ -4,39 +4,37 @@ db = SQLAlchemy()
 
 class Atleta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    cpf = db.Column(db.String(20))
-    data_nascimento = db.Column(db.Date)
-    categoria = db.Column(db.String(20))
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(14), nullable=False)
+    data_nascimento = db.Column(db.String(10), nullable=False)
+    categoria = db.Column(db.String(20), nullable=False)
 
     altura = db.Column(db.String(10))
     endereco = db.Column(db.String(200))
     telefone = db.Column(db.String(20))
-
-    responsavel_nome = db.Column(db.String(100))
-    responsavel_telefone = db.Column(db.String(20))
-
+    nome_responsavel = db.Column(db.String(100))
+    telefone_responsavel = db.Column(db.String(20))
     escola = db.Column(db.String(100))
     local_treino = db.Column(db.String(100))
-
     tamanho_camisa = db.Column(db.String(10))
     numero_camisa = db.Column(db.String(10))
 
-    possui_padrao_jogo = db.Column(db.Boolean)
-    possui_padrao_treino = db.Column(db.Boolean)
+    possui_padrao_jogo = db.Column(db.String(5))
+    possui_padrao_treino = db.Column(db.String(5))
 
 
 class Competicao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    ano = db.Column(db.Integer)
-    limite = db.Column(db.Integer)
-
-    def inscritos_count(self):
-        return Inscricao.query.filter_by(competicao_id=self.id).count()
+    nome = db.Column(db.String(100), nullable=False)
+    ano = db.Column(db.String(10), nullable=False)
+    limite_atletas = db.Column(db.Integer, nullable=False)
 
 
 class Inscricao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
     atleta_id = db.Column(db.Integer, db.ForeignKey('atleta.id'))
     competicao_id = db.Column(db.Integer, db.ForeignKey('competicao.id'))
+
+    atleta = db.relationship('Atleta', backref='inscricoes')
+    competicao = db.relationship('Competicao', backref='inscricoes')
