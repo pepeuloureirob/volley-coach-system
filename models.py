7d7ce6dc3d196d8af1e4b78c4de01fed71600860
pepeuloureirob <pepeu.loureirob@gmail.com>
2026-02-22 22:22:39 -0300

    Update models.py

--- models.py
+++ models.py
@@ -2,58 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# ---------------- ATLETA ----------------
-
 class Atleta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    nome = db.Column(db.String(100), nullable=False)
-    cpf = db.Column(db.String(20), nullable=False)
-    nascimento = db.Column(db.Date, nullable=False)
+    nome = db.Column(db.String(100))
+    cpf = db.Column(db.String(14))
+    data_nascimento = db.Column(db.Date)
     categoria = db.Column(db.String(20))
 
-    altura = db.Column(db.Float)
+    altura = db.Column(db.String(10))
     endereco = db.Column(db.String(200))
-    telefone = db.Column(db.String(30))
+    telefone = db.Column(db.String(20))
 
-    responsavel = db.Column(db.String(100))
-    telefone_responsavel = db.Column(db.String(30))
+    responsavel_nome = db.Column(db.String(100))
+    responsavel_telefone = db.Column(db.String(20))
 
     escola = db.Column(db.String(100))
     local_treino = db.Column(db.String(100))
 
     tamanho_camisa = db.Column(db.String(10))
-    numero_camisa = db.Column(db.Integer)
-
-    padrao_jogo = db.Column(db.Boolean)
-    padrao_treino = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return f"<Atleta {self.nome}>"
-
-# ---------------- COMPETIÇÃO ----------------
+    possui_padrao_jogo = db.Column(db.Boolean)
+    possui_padrao_treino = db.Column(db.Boolean)
+    numero_camisa = db.Column(db.String(5))
 
 class Competicao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    ano = db.Column(db.Integer)
+    limite_atletas = db.Column(db.Integer)
 
-    nome = db.Column(db.String(100), nullable=False)
-    ano = db.Column(db.Integer, nullable=False)
-    limite = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"<Competicao {self.nome} - {self.ano}>"
-
-# ---------------- INSCRIÇÃO ----------------
+    inscricoes = db.relationship('Inscricao', backref='competicao', lazy='dynamic')
 
 class Inscricao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    atleta_id = db.Column(db.Integer, db.ForeignKey('atleta.id'), nullable=False)
-    competicao_id = db.Column(db.Integer, db.ForeignKey('competicao.id'), nullable=False)
-
-    atleta = db.relationship('Atleta', backref='inscricoes')
-    competicao = db.relationship('Competicao', backref='inscricoes')
-
-    def __repr__(self):
-        return f"<Inscricao Atleta {self.atleta_id} - Competicao {self.competicao_id}>"
-        
+    atleta_id = db.Column(db.Integer, db.ForeignKey('atleta.id'))
+    competicao_id = db.Column(db.Integer, db.ForeignKey('competicao.id'))
+    
