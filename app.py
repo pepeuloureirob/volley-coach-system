15d697795828bc7e589eb5002fbe2ca7825404ef
pepeuloureirob <pepeu.loureirob@gmail.com>
2026-02-22 23:40:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from models import db, Atleta, Competicao, Inscricao
-from datetime import date
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -12,107 +12,121 @@ with app.app_context():
     db.create_all()
 
 
-def calcular_categoria(data_nascimento):
-    hoje = date.today()
-    idade = hoje.year - data_nascimento.year
-    subs = [13, 15, 17, 18, 19, 20, 21, 23]
-
-    for sub in subs:
-        if idade <= sub:
-            return f"Sub {sub}"
-    return "Adulto"
-
+# ---------------- HOME / DASHBOARD ---------------- #
 
 @app.route('/')
 def index():
     atletas = Atleta.query.all()
     competicoes = Competicao.query.all()
-    return render_template('index.html', atletas=atletas, competicoes=competicoes)
+    return render_template(
+        'index.html',
+        atletas=atletas,
+        competicoes=competicoes
+    )
 
 
-@app.route('/cadastrar_atleta', methods=['GET', 'POST'])
-def cadastrar_atleta():
-    if request.method == 'POST':
-        nascimento = date.fromisoformat(request.form['data_nascimento'])
+# ---------------- ATLETAS ---------------- #
 
-        atleta = Atleta(
-            nome=request.form['nome'],
-            cpf=request.form['cpf'],
-            data_nascimento=nascimento,
-            categoria=calcular_categoria(nascimento),
-            altura=request.form['altura'],
-            endereco=request.form['endereco'],
-            telefone=request.form['telefone'],
-            responsavel_nome=request.form['responsavel_nome'],
-            responsavel_telefone=request.form['responsavel_telefone'],
-            escola=request.form['escola'],
-            local_treino=request.form['local_treino'],
-            tamanho_camisa=request.form['tamanho_camisa'],
-            numero_camisa=request.form['numero_camisa'],
-            possui_padrao_jogo=request.form['padrao_jogo'] == 'sim',
-            possui_padrao_treino=request.form['padrao_treino'] == 'sim'
-        )
+@app.route('/novo_atleta', methods=['POST'])
+def novo_atleta():
+    atleta = Atleta(
+        nome=request.form['nome'],
+        cpf=request.form['cpf'],
+        data_nascimento=request.form['data_nascimento'],
+        categoria=request.form['categoria'],
+        altura=request.form.get('altura'),
+        endereco=request.form.get('endereco'),
+        telefone=request.form.get('telefone'),
+        nome_responsavel=request.form.get('nome_responsavel'),
+        telefone_responsavel=request.form.get('telefone_responsavel'),
+        escola=request.form.get('escola'),
+        local_treino=request.form.get('local_treino'),
+        tamanho_camisa=request.form.get('tamanho_camisa'),
+        numero_camisa=request.form.get('numero_camisa'),
+        possui_padrao_jogo=request.form.get('possui_padrao_jogo'),
+        possui_padrao_treino=request.form.get('possui_padrao_treino')
+    )
 
-        db.session.add(atleta)
-        db.session.commit()
-        return redirect(url_for('index'))
+    db.session.add(atleta)
+    db.session.commit()
 
-    return render_template('cadastrar_atleta.html')
+    return redirect('/')
 
 
-@app.route('/editar_atleta/<int:id>', methods=['GET', 'POST'])
-def editar_atleta(id):
-    atleta = Atleta.query.get_or_404(id)
-
-    if request.method == 'POST':
-        atleta.nome = request.form['nome']
-        atleta.telefone = request.form['telefone']
-        atleta.altura = request.form['altura']
-        atleta.endereco = request.form['endereco']
-        atleta.numero_camisa = request.form['numero_camisa']
-
-        db.session.commit()
-        return redirect(url_for('index'))
-
-    return render_template('editar_atleta.html', atleta=atleta)
-
-
-@app.route('/remover_atleta/<int:id>')
-def remover_atleta(id):
-    atleta = Atleta.query.get_or_404(id)
+@app.route('/excluir_atleta/<int:id>')
+def excluir_atleta(id):
+    atleta = Atleta.query.get(id)
     db.session.delete(atleta)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect('/')
 
 
-@app.route('/nova_competicao', methods=['GET', 'POST'])
+# ---------------- COMPETIÇÕES ---------------- #
+
+@app.route('/nova_competicao', methods=['POST'])
 def nova_competicao():
-    if request.method == 'POST':
-        comp = Competicao(
-            nome=request.form['nome'],
-            ano=int(request.form['ano']),
-            limite=int(request.form['limite'])
-        )
-        db.session.add(comp)
-        db.session.commit()
-        return redirect(url_for('index'))
+    competicao = Competicao(
+        nome=request.form['nome'],
+        ano=request.form['ano'],
+        limite_atletas=request.form['limite_atletas']
+    )
 
-    return render_template('nova_competicao.html')
+    db.session.add(competicao)
+    db.session.commit()
 
+    return redirect('/')
+
+
+@app.route('/excluir_competicao/<int:id>')
+def excluir_competicao(id):
+    competicao = Competicao.query.get(id)
+    db.session.delete(competicao)
+    db.session.commit()
+    return redirect('/')
+
+
+# ---------------- INSCRIÇÕES ---------------- #
 
 @app.route('/inscrever/<int:atleta_id>/<int:competicao_id>')
 def inscrever(atleta_id, competicao_id):
-    competicao = Competicao.query.get_or_404(competicao_id)
 
-    if competicao.inscritos_count() >= competicao.limite:
-        return "Limite de atletas atingido."
+    existente = Inscricao.query.filter_by(
+        atleta_id=atleta_id,
+        competicao_id=competicao_id
+    ).first()
 
-    inscricao = Inscricao(atleta_id=atleta_id, competicao_id=competicao_id)
-    db.session.add(inscricao)
+    if existente:
+        return redirect('/')
+
+    competicao = Competicao.query.get(competicao_id)
+
+    total = Inscricao.query.filter_by(
+        competicao_id=competicao_id
+    ).count()
+
+    if total >= competicao.limite_atletas:
+        return redirect('/')
+
+    nova = Inscricao(
+        atleta_id=atleta_id,
+        competicao_id=competicao_id
+    )
+
+    db.session.add(nova)
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return redirect('/')
 
+
+@app.route('/remover_inscricao/<int:id>')
+def remover_inscricao(id):
+    inscricao = Inscricao.query.get(id)
+    db.session.delete(inscricao)
+    db.session.commit()
+    return redirect('/')
+
+
+# ---------------- RUN ---------------- #
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
